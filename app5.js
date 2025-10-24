d850c46c16b98b90983ce5a00b28fa83ff831374
Aoyama Takuma <aotaku47h18@gmail.com>
2025-10-24 22:39:28 +0900

    変更

--- app5.js
+++ app5.js
@@ -63,4 +63,39 @@ app.get("/janken", (req, res) => {
   res.render( 'janken', display );
 });
 
+app.get("/janken1024", (req, res) => {
+  let hand = req.query.hand;
+  let win = Number( req.query.win );
+  let total = Number( req.query.total );
+  console.log( {hand, win, total});
+  const num = Math.floor( Math.random() * 3 + 1 );
+  let cpu = '';
+  let judgement = '';
+   if( num==1 ) cpu = 'グー';
+  else if( num==2 ) cpu = 'チョキ';
+  else cpu = 'パー';
+  // ここに勝敗の判定を入れる
+  // 以下の数行は人間の勝ちの場合の処理なので，
+  // 判定に沿ってあいこと負けの処理を追加する
+if (hand == num) {
+    judgement = 'あいこ';
+  } else if (
+    (hand == 1 && num == 2) ||
+    (hand == 2 && num == 3) ||
+    (hand == 3 && num == 1)
+  ) {
+    judgement = '勝ち';
+    win += 1;
+  } else {
+    judgement = '負け';
+  }
+  total += 1;
+
+  
+  
+  const your = ['グー', 'チョキ', 'パー'][hand - 1];
+  
+  res.render('janken1024', { your, cpu, judgement, win, total });
+});
+
 app.listen(8080, () => console.log("Example app listening on port 8080!"));
